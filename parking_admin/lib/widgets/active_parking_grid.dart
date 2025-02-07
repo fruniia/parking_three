162f5652f1375b74c28bbf9ebc6d6223258fba78
Anna Nilsson-Niia <annanilssonniia@gmail.com>
2025-02-07 16:08:41 +0100

    Update views and widgets in admin

--- parking_admin/lib/widgets/active_parking_grid.dart
+++ parking_admin/lib/widgets/active_parking_grid.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:parking_shared_logic/parking_shared_logic.dart';
 import 'package:parking_shared_ui/parking_shared_ui.dart';
 
-
 class ActiveParkingGrid extends StatelessWidget {
   final ParkingProvider parkingProvider;
   final ParkingCostService _costService;
@@ -11,14 +10,17 @@ class ActiveParkingGrid extends StatelessWidget {
     super.key,
     required this.parkingProvider,
     required ParkingCostService costService,
-  })  : _costService = costService;
+  }) : _costService = costService;
 
   @override
   Widget build(BuildContext context) {
     return parkingProvider.activeParkingSessions.isEmpty
-        ? const Center(child: CircularProgressIndicator())
+        ? const Center(
+            child: Text('No active parking sessions',
+                style: TextStyle(fontSize: 22)),
+          )
         : GridView.builder(
-            gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 3,
               crossAxisSpacing: 8.0,
               mainAxisSpacing: 8.0,
@@ -28,7 +30,7 @@ class ActiveParkingGrid extends StatelessWidget {
             itemBuilder: (context, index) {
               var parking = parkingProvider.activeParkingSessions[index];
               double parkingCost = _costService.calculateParkingCost(parking);
-    
+
               return Card(
                 elevation: 4.0,
                 child: Padding(
